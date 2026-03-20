ebb5ed5dd52aeb6ed889d9cd0807d6157310b219
Jon Waldstein <Jpwaldstein@gmail.com>
2026-03-20 16:28:52 -0400

    refactor: remove storage and api v3

--- src/Uplink/Config.php
+++ src/Uplink/Config.php
@@ -4,15 +4,8 @@ namespace StellarWP\Uplink;
 
 use RuntimeException;
 use StellarWP\ContainerContract\ContainerInterface;
-use StellarWP\Uplink\Storage\Contracts\Storage;
-use StellarWP\Uplink\Storage\Drivers\Option_Storage;
 
 class Config {
-
-	/**
-	 * The default authorization cache time in seconds (6 hours).
-	 */
-	public const DEFAULT_AUTH_CACHE = 21600;
 
 	/**
 	 * The default base URL for the StellarWP licensing and catalog API.
@@ -38,21 +31,6 @@ class Config {
 	 * @var string
 	 */
 	protected static $hook_prefix = '';
-
-	/**
-	 * How long in seconds we cache successful authorization
-	 * token requests.
-	 *
-	 * @var int
-	 */
-	protected static $auth_cache_expiration = self::DEFAULT_AUTH_CACHE;
-
-	/**
-	 * The storage driver FQCN to use.
-	 *
-	 * @var class-string<Storage>
-	 */
-	protected static $storage_driver = Option_Storage::class;
 
 	/**
 	 * The base URL for the StellarWP licensing and catalog API.
@@ -139,9 +117,8 @@ class Config {
 	 * @return void
 	 */
 	public static function reset(): void {
-		static::$hook_prefix           = '';
-		static::$auth_cache_expiration = self::DEFAULT_AUTH_CACHE;
-		static::$api_base_url          = self::DEFAULT_API_BASE_URL;
+		static::$hook_prefix = '';
+		static::$api_base_url = self::DEFAULT_API_BASE_URL;
 	}
 
 	/**
@@ -168,51 +145,6 @@ class Config {
 	 */
 	public static function set_hook_prefix( string $prefix ): void {
 		static::$hook_prefix = $prefix;
-	}
-
-	/**
-	 * Set the token authorization expiration.
-	 *
-	 * @param int $seconds  The time seconds the cache will exist for.
-	 *                       -1 = disabled, 0 = no expiration.
-	 *
-	 * @return void
-	 */
-	public static function set_auth_cache_expiration( int $seconds ): void {
-		static::$auth_cache_expiration = $seconds;
-	}
-
-	/**
-	 * Get the token authorization expiration.
-	 *
-	 * @return int
-	 */
-	public static function get_auth_cache_expiration(): int {
-		return static::$auth_cache_expiration;
-	}
-
-	/**
-	 * Set the underlying storage driver.
-	 *
-	 * @param string $class_name The FQCN to a storage driver.
-	 *
-	 * @phpstan-param class-string<Storage> $class_name
-	 *
-	 * @return void
-	 */
-	public static function set_storage_driver( string $class_name ): void {
-		static::$storage_driver = $class_name;
-	}
-
-	/**
-	 * Get the underlying storage driver.
-	 *
-	 * @return class-string<Storage>
-	 */
-	public static function get_storage_driver(): string {
-		$driver = static::$storage_driver;
-
-		return $driver ?: Option_Storage::class;
 	}
 
 	/**
--- tests/wpunit/ConfigTest.php
+++ tests/wpunit/ConfigTest.php
@@ -3,28 +3,8 @@
 namespace StellarWP\Uplink\Tests;
 
 use StellarWP\Uplink\Config;
-use StellarWP\Uplink\Storage\Drivers\Option_Storage;
-use StellarWP\Uplink\Storage\Drivers\Transient_Storage;
 
 final class ConfigTest extends UplinkTestCase {
-
-	public function test_it_gets_and_sets_auth_token_cache_expiration(): void {
-		$this->assertSame( Config::DEFAULT_AUTH_CACHE, Config::get_auth_cache_expiration() );
-
-		Config::set_auth_cache_expiration( DAY_IN_SECONDS );
-
-		$this->assertSame( DAY_IN_SECONDS, Config::get_auth_cache_expiration() );
-	}
-
-	public function test_it_gets_default_storage_driver(): void {
-		$this->assertSame( Option_Storage::class, Config::get_storage_driver() );
-	}
-
-	public function test_it_gets_and_sets_storage_driver(): void {
-		Config::set_storage_driver( Transient_Storage::class );
-
-		$this->assertSame( Transient_Storage::class, Config::get_storage_driver() );
-	}
 
 	public function test_it_gets_default_api_base_url(): void {
 		$this->assertSame( Config::DEFAULT_API_BASE_URL, Config::get_api_base_url() );
