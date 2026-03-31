a58cdb72c901a8794dcb48c479a14ea201e37f5a
Jon Waldstein <Jpwaldstein@gmail.com>
2026-03-31 16:00:58 -0400

    refactor: simplify Resolve_Feature_Collection with internal type map

--- src/Harbor/Features/Provider.php
+++ src/Harbor/Features/Provider.php
@@ -2,15 +2,8 @@
 
 namespace LiquidWeb\Harbor\Features;
 
-use LiquidWeb\Harbor\Catalog\Catalog_Repository;
 use LiquidWeb\Harbor\Contracts\Abstract_Provider;
 use LiquidWeb\Harbor\Features\Strategy\Strategy_Factory;
-use LiquidWeb\Harbor\Features\Types\Feature;
-use LiquidWeb\Harbor\Features\Types\Flag;
-use LiquidWeb\Harbor\Features\Types\Plugin;
-use LiquidWeb\Harbor\Features\Types\Theme;
-use LiquidWeb\Harbor\Licensing\License_Manager;
-use LiquidWeb\Harbor\Site\Data;
 
 /**
  * Registers the Features subsystem in the DI container and hooks.
@@ -29,41 +22,12 @@ class Provider extends Abstract_Provider {
 	public function register(): void {
 		$this->container->singleton( Strategy_Factory::class, Strategy_Factory::class );
 
-		$this->container->singleton(
-			Resolve_Feature_Collection::class,
-			function () {
-				$resolver = new Resolve_Feature_Collection(
-					$this->container->get( Catalog_Repository::class ),
-					$this->container->get( License_Manager::class ),
-					$this->container->get( Data::class )
-				);
-
-				$this->register_default_types( $resolver );
-
-				return $resolver;
-			}
-		);
-
+		$this->container->singleton( Resolve_Feature_Collection::class, Resolve_Feature_Collection::class );
 		$this->container->singleton( Feature_Repository::class, Feature_Repository::class );
 		$this->container->singleton( Feature_Collection::class, Feature_Collection::class );
 		$this->container->singleton( Manager::class, Manager::class );
 
 		$this->container->singleton( Update\Provider::class, Update\Provider::class );
 		$this->container->get( Update\Provider::class )->register();
-	}
-
-	/**
-	 * Registers the default feature type to class mappings.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param Resolve_Feature_Collection $resolver The feature collection resolver.
-	 *
-	 * @return void
-	 */
-	private function register_default_types( Resolve_Feature_Collection $resolver ): void {
-		$resolver->register_type( Feature::TYPE_PLUGIN, Plugin::class );
-		$resolver->register_type( Feature::TYPE_FLAG, Flag::class );
-		$resolver->register_type( Feature::TYPE_THEME, Theme::class );
 	}
 }
