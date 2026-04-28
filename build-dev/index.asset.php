ccb5e94c5ca02bcc2053b95ec95d458ab1760db1
Estevão Costa <estevao90@gmail.com>
2026-04-28 09:20:26 -0300

    Suppress legacy license banner when products are unified-licensed

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f9aa3b974fa45ad17aa5');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3a3acc61a50038d24777');
