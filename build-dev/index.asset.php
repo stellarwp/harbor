3df16c535461693436cdb2890700c2a7d6f4c9b7
pramodjodhani <5794565+pramodjodhani@users.noreply.github.com>
2026-06-05 10:55:25 +0000

    Build frontend assets

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'daa3f53a92b86af3df4d');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '15d9bcfeaaf63bae053a');
