af49f7d3693c43e253a4ebad60b72e38edb113d6
dave-green-uk <3214928+dave-green-uk@users.noreply.github.com>
2026-07-22 12:53:38 +0000

    Build frontend assets

---
+++ build-dev/activation.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ab69f34667398c80fadf');
--- build-dev/index.js
+++ build-dev/index.js
@@ -5740,7 +5740,7 @@ const expiryTextClass = {
  * Keys with two or fewer dash-delimited segments have nothing safe to mask
  * (no middle to hide), so they are returned unchanged.
  *
- * @since TBD
+ * @since 1.5.0
  */
 function maskLicenseKey(key) {
   const segments = key.split('-');
